0402c8b6d687f2943210bf4742971d34ec9540eb
Suman Kumar <kumartankhush786@gmail.com>
2022-04-07 18:04:55 +0530

    First commit Message

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {ChatEngine} from 'react-chat-engine';
+import './App.css'
+import ChatFeed from './components/ChatFeed';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () =>{
+    return(
+        <ChatEngine
+            height='100vh'
+			userName='kumarsuman'
+			userSecret='123123'
+			projectID='91d139b4-5a5b-4deb-972f-9fb185bc8366'
+            renderChatFeed= {(ChatAppProcess)=><ChatFeed {...ChatAppProcess} />}
+         />
+        )
 }
 
 export default App;
